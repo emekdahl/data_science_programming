f9d19e4e22ed33dde8e1cde9c14b284cd9c307aa
emekdahl <emily@otus.com>
2019-01-23 07:38:14 -0600

    distinct solutions

--- sql_for_data_analysis/distinct.sql
+++ sql_for_data_analysis/distinct.sql
@@ -1,11 +1,20 @@
+-- Use DISTINCT to test if there are any accounts associated with more than one region.
+
 SELECT DISTINCT r.name region_name, a.name account_name
 FROM accounts a
 INNER JOIN sales_reps s on a.sales_rep_id = s.id
 INNER JOIN region r on s.region_id = r.id
 ORDER BY a.name;
 
-SELECT DISTINCT s.name sr_name, a.name account_name
+SELECT DISTINCT id, name
+FROM accounts;
+
+-- Have any sales reps worked on more than one account?
+SELECT s.id, s.name sr_name, COUNT(*) num_accounts
 FROM accounts a
 INNER JOIN sales_reps s on a.sales_rep_id = s.id
-INNER JOIN region r on s.region_id = r.id
+GROUP BY s.id, s.name
 ORDER BY s.name;
+
+SELECT DISTINCT id, name
+FROM sales_reps;
