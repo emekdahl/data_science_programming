e0537e4a9f6c725555e8890fc3283f8330f322aa
emekdahl <emily@otus.com>
2019-01-25 06:58:51 -0600

    having quiz - most frequently used channel

--- sql_for_data_analysis/sql_aggregations/having.sql
+++ sql_for_data_analysis/sql_aggregations/having.sql
@@ -60,9 +60,8 @@ LIMIT 1;
 SELECT a.id, a.name, COUNT(w.channel) fb_count
 FROM accounts a
 INNER JOIN web_events w on a.id = w.account_id
-WHERE w.channel = 'facebook'
-GROUP BY a.id, a.name
-HAVING COUNT(w.channel) > 6
+GROUP BY a.id, a.name, w.channel
+HAVING COUNT(w.channel) > 6 AND w.channel = 'facebook'
 ORDER BY fb_count DESC;
 
 -- Which account used facebook most as a channel? 
@@ -75,9 +74,10 @@ ORDER BY fb_count DESC
 LIMIT 1;
 
 -- Which channel was most frequently used by most accounts?
-SELECT w.channel, COUNT(a.id) account_count
+SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
 FROM accounts a
-INNER JOIN web_events w on a.id = w.account_id
-GROUP BY w.channel
-ORDER BY account_count DESC
-LIMIT 1;
+INNER JOIN web_events w 
+ON a.id = w.account_id
+GROUP BY a.id, a.name, w.channel
+ORDER BY use_of_channel DESC
+LIMIT 10;
