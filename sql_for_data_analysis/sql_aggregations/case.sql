23d3ed51ae979af5a3cbdd9c30053922617d2eaa
emekdahl <emily@otus.com>
2019-01-30 07:04:07 -0600

    case quiz - separate accounts by level of total expenditure all time

--- sql_for_data_analysis/sql_aggregations/case.sql
+++ sql_for_data_analysis/sql_aggregations/case.sql
@@ -15,7 +15,16 @@ END
 FROM orders o
 GROUP by 2;
 -- We would like to understand 3 different levels of customers based on the amount associated with their purchases. The top level includes anyone with a Lifetime Value (total sales of all orders) greater than 200,000 usd. The second level is between 200,000 and 100,000 usd. The lowest level is anyone under 100,000 usd. Provide a table that includes the level associated with each account. You should provide the account name, the total sales of all orders for the customer, and the level. Order with the top spending customers listed first.
-
+SELECT a.name, SUM(o.total_amt_usd) as total_sales,
+CASE 
+	WHEN SUM(o.total_amt_usd) > 200000 THEN 'Over 200k' 
+	WHEN SUM(o.total_amt_usd) <= 200000 AND SUM(o.total_amt_usd) >= 100000  THEN 'Between 100k and 200k'
+    WHEN SUM(o.total_amt_usd) < 100000 THEN 'Under 100K'
+END as level
+FROM orders o
+JOIN accounts a on o.account_id = a.id
+GROUP by 1
+ORDER BY total_sales DESC;
 
 -- We would now like to perform a similar calculation to the first, but we want to obtain the total amount spent by customers only in 2016 and 2017. Keep the same levels as in the previous question. Order with the top spending customers listed first.
 
