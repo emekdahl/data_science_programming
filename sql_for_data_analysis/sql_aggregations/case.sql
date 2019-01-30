b5a77bfb3a6c853019ca571fde39070eada14e84
emekdahl <emily@otus.com>
2019-01-30 07:14:16 -0600

    case quiz - find top performing sales reps by order count

--- sql_for_data_analysis/sql_aggregations/case.sql
+++ sql_for_data_analysis/sql_aggregations/case.sql
@@ -40,6 +40,15 @@ GROUP by 1
 ORDER BY total_sales DESC;
 
 -- We would like to identify top performing sales reps, which are sales reps associated with more than 200 orders. Create a table with the sales rep name, the total number of orders, and a column with top or not depending on if they have more than 200 orders. Place the top sales people first in your final table.
-
+SELECT sr.name, SUM(o.id) as total_orders,
+CASE 
+	WHEN SUM(o.id) > 200 THEN 'TOP' 
+    ELSE 'NOT'
+END as top_performer
+FROM orders o
+JOIN accounts a on o.account_id = a.id
+JOIN sales_reps sr on a.sales_rep_id = sr.id
+GROUP by 1
+ORDER BY total_orders DESC;
 
 -- The previous didn't account for the middle, nor the dollar amount associated with the sales. Management decides they want to see these characteristics represented as well. We would like to identify top performing sales reps, which are sales reps associated with more than 200 orders or more than 750000 in total sales. The middle group has any rep with more than 150 orders or 500000 in sales. Create a table with the sales rep name, the total number of orders, total sales across all orders, and a column with top, middle, or low depending on this criteria. Place the top sales people based on dollar amount of sales first in your final table. You might see a few upset sales people by this criteria!
