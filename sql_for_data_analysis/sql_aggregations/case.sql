c9150ad8e882f4e656837e027c30409508d8a342
emekdahl <emily@otus.com>
2019-01-30 08:25:34 -0600

    case quiz - correct sum order id to order count star and add column name again

--- sql_for_data_analysis/sql_aggregations/case.sql
+++ sql_for_data_analysis/sql_aggregations/case.sql
@@ -52,10 +52,10 @@ GROUP by 1
 ORDER BY total_orders DESC;
 
 -- The previous didn't account for the middle, nor the dollar amount associated with the sales. Management decides they want to see these characteristics represented as well. We would like to identify top performing sales reps, which are sales reps associated with more than 200 orders or more than 750000 in total sales. The middle group has any rep with more than 150 orders or 500000 in sales. Create a table with the sales rep name, the total number of orders, total sales across all orders, and a column with top, middle, or low depending on this criteria. Place the top sales people based on dollar amount of sales first in your final table. You might see a few upset sales people by this criteria!
-SELECT sr.name, SUM(o.id) as total_orders, SUM(o.total_amt_usd) as total_sales,
+SELECT sr.name, COUNT(*) as total_orders, SUM(o.total_amt_usd) as total_sales,
 CASE 
-	WHEN SUM(o.id) > 200 OR SUM(o.total_amt_usd) > 750000 THEN 'TOP' 
-    WHEN SUM(o.id) > 200 OR SUM(o.total_amt_usd) > 500000 THEN 'MIDDLE'
+	WHEN  COUNT(*) > 200 OR SUM(o.total_amt_usd) > 750000 THEN 'TOP' 
+    WHEN  COUNT(*) > 200 OR SUM(o.total_amt_usd) > 500000 THEN 'MIDDLE'
     ELSE 'LOW'
 END as top_performer
 FROM orders o
