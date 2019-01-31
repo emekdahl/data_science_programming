93b8468a1fef18ea6b7a6d445c73ed9a887842bc
emekdahl <emily@otus.com>
2019-01-31 06:31:34 -0600

    entire first month of orders average paper type qty per order

--- sql_for_data_analysis/sub_queries/more_on_subqueries.sql
+++ sql_for_data_analysis/sub_queries/more_on_subqueries.sql
@@ -5,4 +5,17 @@ GROUP BY 1
 ORDER BY 1
 LIMIT 1;
 
---
+-- entire first month of orders average order of various paper types
+SELECT AVG(o.standard_qty) as standard_avg, AVG(o.gloss_qty) as gloss_avg, AVG(o.poster_qty) as poster_avg
+    FROM orders o
+    WHERE DATE_TRUNC('month', o.occurred_at) = (
+        SELECT DATE_TRUNC('month', o.occurred_at) as min_month
+            FROM orders o
+            ORDER BY min_month
+            LIMIT 1)
+
+                                                                                                       
+
+
+                                                                                                       
+
