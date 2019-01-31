332db3386511baadbf879c4bfdaa721db30b697c
emekdahl <emily@otus.com>
2019-01-31 06:34:50 -0600

    use min function to simplify subquery

--- sql_for_data_analysis/sub_queries/more_on_subqueries.sql
+++ sql_for_data_analysis/sub_queries/more_on_subqueries.sql
@@ -6,13 +6,10 @@ ORDER BY 1
 LIMIT 1;
 
 -- entire first month of orders average order of various paper types
-SELECT AVG(o.standard_qty) as standard_avg, AVG(o.gloss_qty) as gloss_avg, AVG(o.poster_qty) as poster_avg
-    FROM orders o
-    WHERE DATE_TRUNC('month', o.occurred_at) = (
-        SELECT DATE_TRUNC('month', o.occurred_at) as min_month
-            FROM orders o
-            ORDER BY min_month
-            LIMIT 1)
+SELECT AVG(standard_qty) as standard_avg, AVG(gloss_qty) as gloss_avg, AVG(poster_qty) as poster_avg
+    FROM orders
+    WHERE DATE_TRUNC('month', occurred_at) = (
+        SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
 
                                                                                                        
 
