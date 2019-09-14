673f820ece7d6b5bb444d92a2b27626faa04cdc8
emekdahl <emily@otus.com>
2019-09-14 06:51:03 -0500

    string interpolation

---
+++ intro_to_python/string_interpolation.py
@@ -0,0 +1,7 @@
+# Write two lines of code below, each assigning a value to a variable
+dog_breed = "Shiba Inu"
+dog_name = "Dakota"
+
+# Now write a print statement using .format() to print out a sentence and the 
+#   values of both of the variables
+print("{} is a {}".format(dog_name, dog_breed))
