b5901647037c8a825ee14ce4dd0171ec351f0631
emekdahl <emily@otus.com>
2019-09-18 04:49:39 -0500

    count XML tags

---
+++ intro_to_python/html_tags.py
@@ -0,0 +1,9 @@
+items = ['first string', 'second string']
+html_str = "<ul>\n"          # The "\n" here is the end-of-line char, causing
+                             # chars after this in html_str to be on next line
+
+for item in items:
+    html_str += "<li>{}</li>\n".format(item)
+html_str += "</ul>"
+
+print(html_str)
