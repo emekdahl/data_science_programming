10098c5a9264516e50456e1b59aae0461460456c
emekdahl <emily@otus.com>
2019-09-18 06:23:54 -0500

    more efficient solution

--- intro_to_python/news_ticker.py
+++ intro_to_python/news_ticker.py
@@ -1,4 +1,3 @@
-# HINT: modify the headlines list to verify your loop works with different inputs
 headlines = ["Local Bear Eaten by Man",
              "Legislature Announces New Laws",
              "Peasant Discovers Violence Inherent in System",
@@ -7,21 +6,10 @@ headlines = ["Local Bear Eaten by Man",
              "Papperbok Review: Totally Triffic"]
 
 news_ticker = ""
-# write your loop here
-length = len(news_ticker)
-items = []
 for headline in headlines:
-    print("current length: {}".format(length))
-    if length >= 140:
-        print("  breaking from the loop now!")
+    news_ticker += headline + " "
+    if len(news_ticker) >= 140:
+        news_ticker = news_ticker[:140]
         break
-    elif length < 140:
-        headline_length = len(headline)
-        if length + headline_length > 140:
-            index_needed = 140 - length -1
-            headline = headline[0:index_needed]
-        news_ticker = news_ticker + " " + headline
-        length = len(news_ticker)
-
 
 print(news_ticker)
