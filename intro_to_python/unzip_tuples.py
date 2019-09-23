4f6b930f4b3ade7a37f23d625e69f35671085fb0
emekdahl <emily@otus.com>
2019-09-23 07:05:09 -0500

    unzip tuples

---
+++ intro_to_python/unzip_tuples.py
@@ -0,0 +1,10 @@
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
+
+# define names and heights here
+names = []
+heights = []
+
+names, heights = zip(*cast)
+
+print(names)
+print(heights)
