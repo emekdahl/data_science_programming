7bd3d9164c248aa5539b405a1ff9701d385f933e
emekdahl <emily@otus.com>
2019-09-23 06:45:31 -0500

    better way to check primes

--- intro_to_python/prime_numbers.py
+++ intro_to_python/prime_numbers.py
@@ -1,25 +1,16 @@
-## Your code should check if each number in the list is a prime number
 check_prime = [26, 39, 51, 53, 57, 79, 85]
 
-## write your code here
-## HINT: You can use the modulo operator to find a factor
-
+# iterate through the check_prime list
 for num in check_prime:
-    isPrime = True
-    message = ""
-    counter = num
-    
-    while counter > 1:
-        if num % counter == 0:
-            factor = num
-            isPrime = False
+
+# search for factors, iterating through numbers ranging from 2 to the number itself
+    for i in range(2, num):
+
+# number is not prime if modulo is 0
+        if (num % i) == 0:
+            print("{} is NOT a prime number, because {} is a factor of {}".format(num, i, num))
             break
-        else:
-            counter -=1
-    
-    if isPrime:
-        message = "{} is a prime number".format(num)
-    else:
-        message = "{} is not a prime number. {} is a factor of {}".format(num, factor, num)
-        
-print(message)
+
+# otherwise keep checking until we've searched all possible factors, and then declare it prime
+        if i == num -1:    
+            print("{} IS a prime number".format(num))
