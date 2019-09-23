3105a2154642cdef4ab53169cf395321a51dccf6
emekdahl <emily@otus.com>
2019-09-23 06:55:18 -0500

    zip dictionary

---
+++ intro_to_python/zip_dictionary.py
@@ -0,0 +1,8 @@
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+cast = {}
+for name, height in zip(cast_names, cast_heights):
+    cast.update({'{}'.format(name): height})
+    
+print(cast)
