4b6087a192f74eb97cd31c89ceab67889fc9daf1
emekdahl <emily@otus.com>
2019-09-27 06:16:38 -0500

    more numpy practice

---
+++ intro_to_python/numpy_practice.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# create numpy array of letters a-j
+letter_array = np.array(["a","b","c","d","e","f","g","h","i","j"])
+print("Letter Array: ", letter_array)
+
+# get dtype of array
+print("dtype: ", letter_array.dtype)
+
+# get shape of array
+print("shape: ", letter_array.shape)
+
+# get size of array
+print("size: ", letter_array.size)
